64d18e4da137ba3cf3bc963c8336b06574258f24
yuza <hi1de2osan3@gmail.com>
2022-09-16 14:01:36 +0900

    kousinn

--- Iteam3ninn/Project/GameProject/Game/Bullet.cpp
+++ Iteam3ninn/Project/GameProject/Game/Bullet.cpp
@@ -3,7 +3,7 @@
 #include "Enemy.h"
 #include "Map.h"
 
-Bullet::Bullet(int type, const CVector2D& pos, float ang, float speed):Base(type) {
+Bullet::Bullet(int type, const CVector2D& pos, float ang, float speed) :Base(type) {
 	if (type == eType_Player_Bullet)
 		m_img = COPY_RESOURCE("Bullet", CImage);
 	else
@@ -11,35 +11,50 @@ Bullet::Bullet(int type, const CVector2D& pos, float ang, float speed):Base(type
 
 	m_pos = pos;
 	m_img.SetCenter(16, 16);
+	m_rad = 8;
 	m_ang = ang;
 	m_speed = speed;
+	m_cnt = 2;
+	m_vec = CVector2D(sin(m_ang), cos(m_ang)) * m_speed;
 }
 
-Bullet::~Bullet(){
+Bullet::~Bullet() {
 
 }
 
-void Bullet::Update(){
+void Bullet::Update() {
 	//ˆÚ“®‘O‚ÌˆÊ’u
 	m_pos_old = m_pos;
-	m_vec = CVector2D(sin(m_ang), cos(m_ang)) * m_speed;
 	m_pos += m_vec;
 
 }
 
-void Bullet::Draw(){
+void Bullet::Draw() {
 	m_img.SetPos(m_pos);
 	m_img.SetAng(m_ang);
 	m_img.Draw();
 }
 
-void Bullet::Collision(Base* b){
-	switch (b->m_type){
+void Bullet::Collision(Base* b) {
+	switch (b->m_type) {
 	case eType_Field:
 		if (Map* m = dynamic_cast<Map*>(b)) {
-			int t = m->CollisionMap(m_pos);
-			if (t != 0)
+			int t = m->CollisionMap(CVector2D(m_pos.x, m_pos_old.y));
+			if (t != 0) {
+				m_pos.x = m_pos_old.x;
+				m_vec.x *= -1;
+				m_cnt--;
+			}
+			t = m->CollisionMap(CVector2D(m_pos_old.x, m_pos.y));
+			if (t != 0) {
+				m_pos.y = m_pos_old.y;
+				m_vec.y *= -1;
+				m_cnt--;
+			}
+			if (m_cnt == 0) {
 				SetKill();
+				m_cnt = 2;
+			}
 		}
 		break;
 	case eType_Player:
@@ -47,9 +62,28 @@ void Bullet::Collision(Base* b){
 			SetKill();
 			b->SetKill();
 		}
+
+		if (m_type == eType_Player_Bullet && Base::CollisionCircle(this, b) && m_cnt == 1) {
+			SetKill();
+			b->SetKill();
+		}
 		break;
 	case eType_Enemy:
-		if(m_type == eType_Player_Bullet && Base::CollisionCircle(this, b)) {
+		if (m_type == eType_Player_Bullet && Base::CollisionCircle(this, b)) {
+			SetKill();
+			b->SetKill();
+		}
+		break;
+
+	case eType_Player_Bullet:
+		if (m_type == eType_Player_Bullet && Base::CollisionCircle(this, b)) {
+			SetKill();
+			b->SetKill();
+		}
+		break;
+
+	case eType_Enemy_Bullet:
+		if (m_type == eType_Player_Bullet && Base::CollisionCircle(this, b)) {
 			SetKill();
 			b->SetKill();
 		}
--- Iteam3ninn/Project/GameProject/Game/Bullet.h
+++ Iteam3ninn/Project/GameProject/Game/Bullet.h
@@ -4,6 +4,7 @@
 class Bullet : public Base {
 	CImage m_img;
 	float m_speed;
+	int m_cnt;
 public:
 	Bullet(int type, const CVector2D& pos, float ang, float speed);
 	~Bullet();
